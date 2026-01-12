0e785017c6e48348dfd8d07b4ed9dc14197a1e2e
magie99 <github_46432@maxi-login.de>
2026-01-12 17:18:20 +0100

    almost done

--- calculator.js
+++ calculator.js
@@ -1,21 +1,28 @@
 function operate(a,b, operator){
     a = Number(a);
     b = Number(b);
+    let result;
     switch(operator){
         case "+":
-            return a + b;
+             result = a + b;
+             break;
         case "-":
-            return a - b;
+            result = a - b;
+            break;
         case "/":
-            return a / b;
+            result = a / b;
+            break;
         case "*":
-            return a * b;
+            result = a * b;
+            break;
     }
+    return(Math.round(result * 10000)/10000);
 };
 
 const screen = document.querySelector("#screen");
 const btns = document.querySelectorAll("button");
 let operatorCount = 0;
+let floatCount = 0;
 
 btns.forEach(btn => {
     btn.addEventListener("click", function() {
@@ -30,6 +37,7 @@ function updateScreen(input){
         if(operatorCount == 0){
             screen.innerHTML += ` ${input} `
             operatorCount = 1;
+            floatCount = 0;
         }
         else{
             inputs = screen.innerHTML.split(" ");
@@ -41,6 +49,22 @@ function updateScreen(input){
         inputs = screen.innerHTML.split(" ");
         screen.innerHTML = operate(inputs[0],inputs[2], inputs[1]);
         operatorCount = 0;
+        floatCount = 0;
+
+    }
+    else if(input == "."){
+        if (floatCount === 0){
+            screen.innerHTML += input;
+            floatCount = 1;
+        }
+    }
+    else if(input == "b"){
+        screen.innerHTML = screen.innerHTML.slice(0, -1);
+    }
+    else if(input == "c"){
+        screen.innerHTML = "";
+        operatorCount = 0;
+        floatCount = 0;
     }
     else{
         screen.innerHTML += input;
